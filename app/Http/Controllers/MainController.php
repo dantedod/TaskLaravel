22dd70532da184ad35059fed41e875ef160546b3
Dantedod <danted2300@gmail.com>
2025-02-20 20:44:29 -0300

    003

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -4,21 +4,4 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
-class MainController extends Controller
-{
-  public function index($value)
-  {
-    return view('main', ["value" => $value]);
-  }
-
-  public function page2($value)
-  {
-    return view('page2', ["value" => $value]);
-  }
-
-
-  public function page3($value)
-  {
-    return view('page3', ["value" => $value]);
-  }
-}
+class MainController extends Controller {}
