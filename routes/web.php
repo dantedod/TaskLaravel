5a056be5d880e1acc84a24946034cdacb5deeef3
Dantedod <danted2300@gmail.com>
2025-02-20 21:13:49 -0300

    005

--- routes/web.php
+++ routes/web.php
@@ -1,4 +1,10 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use App\Http\Controllers\MainController;
 use Illuminate\Support\Facades\Route;
+
+//auth routers
+Route::get("/login", [AuthController::class, "login"]);
+
+Route::get("/logout", [AuthController::class, "logout"]);
