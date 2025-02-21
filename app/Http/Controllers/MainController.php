3ddaee7a69108fc1c90801484b766d5a9a7061e8
Dantedod <danted2300@gmail.com>
2025-02-21 15:30:24 -0300

    011

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -7,5 +7,13 @@ use Illuminate\Http\Request;
 class MainController extends Controller
 {
 
-  public function index() {}
+  public function index()
+  {
+    echo 'I inside the app';
+  }
+
+  public function newNote()
+  {
+    echo 'i create a the new route';
+  }
 }
