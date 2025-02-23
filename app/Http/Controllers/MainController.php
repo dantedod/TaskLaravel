5f7bba0177a06c49bcd23c2b3c2daec6a3616dbe
Dantedod <danted2300@gmail.com>
2025-02-23 18:51:36 -0300

    Use Services for Decrypt

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\User;
+use App\Services\Operations;
 use Illuminate\Contracts\Encryption\DecryptException;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Crypt;
@@ -28,27 +29,13 @@ class MainController extends Controller
   public function editNote($id)
   {
 
-    $id = $this->decryptId($id);
+    $id = Operations::decryptId($id);
     echo "I'm editing note with id:" . $id;
   }
 
   public function deleteNote($id)
   {
-    $id = $this->decryptId($id);
+    $id = Operations::decryptId($id);
     echo "I'm try to delete the note with id:" . $id;
-  }
-
-  private function decryptId($id)
-  {
-
-    try {
-      //code...
-      $id = Crypt::decrypt($id);
-    } catch (DecryptException $e) {
-
-      return redirect()->route('home');
-    }
-
-    return $id;
   }
 }
