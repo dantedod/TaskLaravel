a2d4211c7abbe2314c5834a57fd91f93b9a9c3d1
Dantedod <danted2300@gmail.com>
2025-02-23 20:59:46 -0300

    more delete forms

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -137,8 +137,15 @@ class MainController extends Controller
     //$note->delete();
 
     //2. softdelete
-    $note->deleted_at = date('Y:m:d H:i:s');
-    $note->save();
+    //$note->deleted_at = date('Y:m:d H:i:s');
+    //$note->save();
+
+    //3.soft delete(property SoftDelets in model)
+    $note->delete();
+
+    //4.hard delete(property SoftDelets in model)
+    //$note->forceDelete();
+
     //redirect to home
     return redirect()->route('home');
   }
